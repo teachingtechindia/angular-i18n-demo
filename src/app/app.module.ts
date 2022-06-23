deac83348703d1642b0bc5718f4b6247e56e8c3c
WeWake <vvakekumar@gmail.com>
2022-06-24 00:00:36 +0530

    i18n angular way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'hi-IN' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
